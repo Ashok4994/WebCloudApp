8eb1b96d767c5b9df92160b8d8f972e9c23930c4
Ashok4994 <thota.ashokchakravarthy@gmail.com>
2020-11-07 15:36:39 -0600

    First commit

---
+++ WebCloudApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Logging;
+
+namespace WebCloudApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        private static readonly string[] Days = new[]
+        {
+            "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"
+        };
+
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("days")]
+        public IEnumerable<string> GetWeeks()
+        {
+            return Days;
+        }
+
+        [HttpGet]
+        [Route("ping")]
+        public string Ping()
+        {
+            return $"Ping is working as of {DateTime.Now}" ;
+        }
+    }
+}
